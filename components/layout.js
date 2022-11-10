aaa3e12f43329244cd4e6e24af3101fb8287774a
chad-cummings-rigil <chad@rigil.com>
2022-11-09 20:07:45 -0600

    updated name

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Chad Cummings';
+const name = 'New Development';
 
 export const siteTitle = 'Next.js Sample Website';
 
